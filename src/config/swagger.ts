8eb9694de66160b6ec5f9c9fefeb475f347a8303
roxanatera <jrnt17@gmail.com>
2024-11-28 10:39:48 +0100

    Corrgiendo error dcocumentacion swwager 1.

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -22,12 +22,35 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 export const setupSwagger = (app: Application): void => {
+  const PORT = process.env.PORT || 10000; 
+
+  const swaggerOptions = {
+      definition: {
+          openapi: "3.0.0",
+          info: {
+              title: "To-Do App API",
+              version: "1.0.0",
+              description: "API para gestionar tareas y usuarios.",
+          },
+          servers: [
+              {
+                  url: process.env.NODE_ENV === "production"
+                      ? "https://todo-app-backend-eef5.onrender.com"
+                      : `http://localhost:${PORT}`
+              },
+          ],
+      },
+      apis: ["./src/routes/*.ts"], // Ruta donde están definidas tus rutas
+  };
+
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-  const serverUrl =
-  process.env.NODE_ENV === "production"
-      ? "https://todo-app-backend-eef5.onrender.com/api-docs" 
-      : "http://localhost:10000/api-docs"; 
+
+  const serverUrl = process.env.NODE_ENV === "production"
+      ? "https://todo-app-backend-eef5.onrender.com/api-docs"
+      : `http://localhost:${PORT}/api-docs`;
 
   console.log(`Documentación de API disponible en: ${serverUrl}`);
 };
+
 
