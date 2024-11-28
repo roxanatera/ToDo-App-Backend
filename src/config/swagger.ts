92e639fb68ef8a9bc897021e3038b5a5205a6f66
roxanatera <jrnt17@gmail.com>
2024-11-28 11:02:06 +0100

    Corrgiendo error documentacion swwager y rutas2.

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,49 +1,31 @@
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-import { Application } from "express";
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Application } from 'express';
 
 const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "To-Do App API",
-      version: "1.0.0",
-      description: "API para gestionar tareas y usuarios.",
-    },
-    servers: [
-      {
-        url: "https://todo-app-backend-eef5.onrender.com/",
-      },
-    ],
-  },
-  apis: ["./src/routes/**/*.ts"]
-, 
-};
-
-export const setupSwagger = (app: Application): void => {
-  const swaggerOptions = {
     definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "To-Do App API",
-        version: "1.0.0",
-        description: "API para gestionar tareas y usuarios.",
-      },
-      servers: [
-        {
-          url: process.env.NODE_ENV === "production"
-            ? "https://todo-app-backend-eef5.onrender.com"
-            : `http://localhost:${process.env.PORT || 5000}`
+        openapi: '3.0.0',
+        info: {
+            title: 'To-Do App API',
+            version: '1.0.0',
+            description: 'API para gestionar tareas y usuarios.',
         },
-      ],
+        servers: [
+            {
+                url: 'https://todo-app-backend-eef5.onrender.com/',
+            },
+        ],
     },
-    apis: ["./src/routes/*.ts"], // Ruta donde están definidas tus rutas
+    apis: ['./src/routes/*.ts'], 
   };
 
-  const swaggerDocs = swaggerJsDoc(swaggerOptions);
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-  console.log(`Documentación de API disponible en: ${swaggerOptions.definition.servers[0].url}/api-docs`);
+export const setupSwagger = (app: Application): void => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+    const serverUrl = process.env.NODE_ENV === 'production'
+        ? 'https://todo-app-backend-eef5.onrender.com/api-docs'
+        : `http://localhost:${process.env.PORT || 5000}/api-docs`;
+
+    console.log(`Documentación de API disponible en: ${serverUrl}`);
 };
-
-
